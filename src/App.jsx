efcf2613c55c79ef030974b78683b3255f2d7d09
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-16 15:28:44 +0000

    I render the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import Nav from "./Components/NavBar/Nav";
+import { Routes, Route } from "react-router-dom";
+import HomPage from "./Pages/Home/Hompage";
 
-import Nav from './Components/NavBar/Nav';
-
- const App =() => {
-
-
-  return (
+const App = () => {
+	return (
 		<>
 			<div className="font-Poppins">
 				<Nav />
+				<Routes>
+					<Route exact path="/" element={<HomPage />} />
+				</Routes>
 			</div>
 		</>
 	);
-}
+};
 
-export default App
+export default App;
