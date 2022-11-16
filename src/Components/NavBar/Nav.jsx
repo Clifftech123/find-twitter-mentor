fb75ebc48a5dd8b4a298e9e9d797cd17a484b0db
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-16 15:29:51 +0000

    New icons was and dark and ligth mode functions was added

--- src/Components/NavBar/Nav.jsx
+++ src/Components/NavBar/Nav.jsx
@@ -1,26 +1,88 @@
-import { Fragment, useState } from "react";
-import { BsMoonStarsFill } from "react-icons/bs";
+import {  useState,useEffect } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { FcAbout } from "react-icons/fc";
 import { AiTwotoneContacts} from "react-icons/ai";
 import { SiStackshare } from "react-icons/si";
-import { CiDark } from "react-icons/ci";
-import { FiSun } from "react-icons/fi";
 import { AiOutlineTeam } from "react-icons/ai";
 
 
-import { Menu, Transition } from "@headlessui/react";
+
 
  const Nav =() => {
-  const [mobileNavOpen, setMobileNavOpen] = useState(false);
-  return (
+	 const [ mobileNavOpen, setMobileNavOpen ] = useState( false );
+	 const [ theme, setTheme ] = useState( 
+		 localStorage.getItem( "theme" ) ? localStorage.getItem( "theme") :"system"
+	  );
+	 const element = document.documentElement
+	 const darkQuery = window.matchMedia(('prefer-color-scheme:dark'))
+
+	//  Light and dark theme ions
+	 const options = [
+		 {
+			 icons: "sunny",
+			 text: "light"
+		 },
+		 {
+			 icons: "moon",
+			 text: "dark"
+		 },
+		 {
+			 icons: "desktop-outline",
+			 text:"system"
+		 }
+	 ];
+
+//    Function to detect system color preference
+	 function onWindMatch() {
+		 if ( localStorage.theme === 'dark' || ( !( 'theme' in localStorage ) && darkQuery.matches ) )
+		 {
+			element.classList.add('dark') 
+		 } else {
+			element.classList.remove("dark");  
+		 }
+	 };
+
+
+//    USE Effect that handle  Light and Dark theme
+	 useEffect(() => {
+		 switch (theme) {
+				case "dark":
+					element.classList.add("dark");
+					localStorage.setItem("theme", "dark");
+					break;
+				case "light":
+					element.classList.remove("dark");
+					localStorage.setItem("theme", "light");
+					break;
+
+				default:
+					localStorage.removeItem("theme");
+					// Invoking the function
+					onWindMatch();
+					break;
+			}
+		
+	 }, [ theme ] );
+
+	// Event lister to listing to the system auto color preference
+	 darkQuery.addEventListener( 'change', ( e ) => {
+		 if ( !( 'theme' in localStorage ) ) {
+			 if ( darkQuery.matches ) {
+				 element.classList.add('dark');
+			 } else {
+				 element.classList.remove('dark')
+			 }
+		 }
+	 })
+	 
+   return (
 		<>
 			{/* Page Container */}
 			<nav>
 				{/* Page Header */}
 				<header
 					id="page-header"
-					className="flex flex-none items-center bg-white shadow-sm z-1"
+					className="flex flex-none items-center dark:text-gray-100 dark:bg-slate-900 duration-100 bg-white shadow-sm z-1"
 				>
 					<div className="container xl:max-w-7xl mx-auto px-4 lg:px-8">
 						<div className="flex justify-between py-4">
@@ -94,67 +156,18 @@ import { Menu, Transition } from "@headlessui/react";
 								</nav>
 								{/* END Desktop Navigation */}
 
-								{/* User Dropdown */}
-								<Menu as="div" className="relative inline-block">
-									{/* Dropdown Toggle Button */}
-									<Menu.Button className="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-3 mr-5  md:ml-20 md:mr-0 py-2 leading-5 text-sm rounded  bg-white text-gray-800 shadow-sm hover:text-gray-800 hover:bg-gray-100 hover:border-gray-300 hover:shadow focus:ring focus:ring-gray-500 focus:ring-opacity-25 active:bg-white active:shadow-none">
-										{/* Them icon */}
-										<BsMoonStarsFill />
-									</Menu.Button>
-									{/* END Dropdown Toggle Button */}
-
-									{/* Dropdown */}
-									<Transition
-										as={Fragment}
-										enter="transition ease-out duration-150"
-										enterFrom="transform opacity-0 scale-75"
-										enterTo="transform opacity-100 scale-100"
-										leave="transition ease-in duration-100"
-										leaveFrom="transform opacity-100 scale-100"
-										leaveTo="transform opacity-0 scale-75"
-									>
-										<Menu.Items className="absolute right-0 origin-top-right mt-2 w-48 shadow-xl rounded z-1">
-											<div className="bg-white ring-1  ring-black ring-opacity-5 rounded divide-y divide-gray-100">
-												<div className="p-2 space-y-1">
-													<Menu.Item>
-														{({ active }) => (
-															<button
-																type="button"
-																className={`flex items-center space-x-2 rounded py-2 px-3 text-sm font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-700 ${
-																	active
-																		? "text-gray-700 bg-gray-100"
-																		: "text-gray-600 hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:bg-gray-100 focus:text-gray-700"
-																}`}
-															>
-																<CiDark className="hi-solid hi-inbox inline-block w-5 h-5 opacity-50" />
-																<span>Dark </span>
-															</button>
-														)}
-													</Menu.Item>
-												</div>
-												<div className="p-2 space-y-1">
-													<Menu.Item>
-														{({ active }) => (
-															<button
-																type="button"
-																className={`flex items-center space-x-2 rounded py-2 px-3 text-sm font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-700 ${
-																	active
-																		? "text-gray-700 bg-gray-100"
-																		: "text-gray-600 hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:bg-gray-100 focus:text-gray-700"
-																}`}
-															>
-																<FiSun className="hi-solid hi-inbox inline-block w-5 h-5 opacity-50" />
-																<span>Light </span>
-															</button>
-														)}
-													</Menu.Item>
-												</div>
-											</div>
-										</Menu.Items>
-									</Transition>
-									{/* END Dropdown */}
-								</Menu>
-								{/* END User Dropdown */}
+								{/* LIGHT AND  DARK MODE */}
+								<div className="relative inline-block">
+								  <div className="inline-flex justify-center items-center space-x-5  font-semibold focus:outline-none px-3 mr-10  md:ml-20 md:mr-0 py-2 leading-5 text-2xl rounded  bg-white text-gray-800    ">
+									  {/* Looping through all the icons */}
+										{options.map((opt) => (
+											<button   onClick={()=> setTheme(opt.text)}  key={opt.text} className={`h-8 w-8 ${theme === opt.text && "text-sky-600" }  `} >
+												<ion-icon name={opt.icons}></ion-icon>
+											</button>
+										))}
+									</div>
+								</div>
+								{/*END LIGHT AND  DARK MODE */}
 
 								{/* Toggle Mobile Navigation */}
 								<div className="lg:hidden">
