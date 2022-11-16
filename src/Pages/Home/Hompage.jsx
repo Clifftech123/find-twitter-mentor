4ce56e5e137b2def2b2a85ceaee6ef0bad0f1ea6
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-16 15:25:55 +0000

    I crated hompeage and render render hero section

---
+++ src/Pages/Home/Hompage.jsx
@@ -0,0 +1,14 @@
+// Ths components  render all the homepage content
+
+import React from "react";
+import Hero from "../../Components/HeroSection/Hero";
+
+const HomPage = () => {
+	return (
+		<>
+			<Hero />
+		</>
+	);
+};
+
+export default HomPage;
