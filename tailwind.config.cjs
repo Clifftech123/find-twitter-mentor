2602d1ee73e9b6748b98d0ec9e83196e0fcb7dcb
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-17 02:17:28 +0000

    I added new images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
-	darkMode:'class',
+	mode: "jit",
+	darkMode: "class",
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {
 				Poppins: "Poppins",
+			},
+			backgroundImage: {
+				Hero: "url('assets/Hero.avif')",
+				
 			},
 		},
 	},
