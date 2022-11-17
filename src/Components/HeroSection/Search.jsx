08753d12e0fb072bd46ddb6313316ab770f5671e
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-17 01:21:16 +0000

    I modfied search btton

--- src/Components/HeroSection/Search.jsx
+++ src/Components/HeroSection/Search.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-import { BsSearch } from "react-icons/bs";
-import { AiOutlineSearch } from "react-icons/ai";
+// This is Search button that wil user search base on the content they want to se 
 
 const Search = () => {
 	return (
 		<>
-			<section className=" px-10 md:px-20 py-10 ">
-				<form className="flex items-center ">
-					<div className="relative w-full">
-						<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-							<BsSearch className="w-5 h-5 text-gray-500 " />
+		
+				<form>
+					<div className="flex px-10 md:px-60 py-10 ">
+						<div className="relative w-full">
+							<input
+								type="search"
+								className="block p-2.5 w-full  text-sm text-gray-900 bg-gray-20  rounded-2xl border-l-gray-50 border-l-2 border border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white active:outline-none focus:outline-none dark:focus:border-blue-500"
+								placeholder="Search Mockups, Logos, Design Templates..."
+								required
+							/>
+							<button
+								type="submit"
+								className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								<svg
+									aria-hidden="true"
+									className="w-5 h-5"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+									></path>
+								</svg>
+								<span className="sr-only">Search</span>
+							</button>
 						</div>
-
-						<input
-							type="text"
-							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 active:outline-none active:border-none block w-full pl-10 p-3  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-							placeholder="Search Mentor: Frontend ,Backend, Full stack"
-							require />
-						
-					
 					</div>
-
-					{/*Search button  */}
-					<button
-						type="submit"
-						className="flex items-center py-3 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
-						<AiOutlineSearch className="w-5 h-5 mr-2 -ml-1" />
-						Search
-					</button>
 				</form>
-			</section>
 		</>
 	);
 };
