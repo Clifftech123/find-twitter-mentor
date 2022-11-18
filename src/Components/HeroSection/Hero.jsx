55565d701ec67bd4d2cbc841f2effbf9ee368399
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-18 15:49:56 +0000

    I added react-typed text effect

--- src/Components/HeroSection/Hero.jsx
+++ src/Components/HeroSection/Hero.jsx
@@ -2,16 +2,16 @@
 
 import React from 'react'
 import Content from './Content'
-
+import Text from './Text';
 
 
 const Hero = () => {
   return (
 		<>
-			
+			Adding background image to the search button
 			<div class="w-full bg-center bg-cover bg-Hero  h-[28rem]">
         <div class=" h-full bg-gray-900 bg-opacity-50">
-        
+          <Text/>
 				</div>
 			</div>
       {/* End of  the search button component */}
