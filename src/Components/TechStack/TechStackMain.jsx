b108f8022f3f88ba0729b79089d52b7bac412455
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-21 09:13:00 +0000

    Creating of Commmunities component

---
+++ src/Components/TechStack/TechStackMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TechStackMain = () => {
+  return (
+      <div className='container text-center'>
+          Welcome to Tech stack.This page is still under services
+    </div>
+  )
+}
+
+export default TechStackMain
