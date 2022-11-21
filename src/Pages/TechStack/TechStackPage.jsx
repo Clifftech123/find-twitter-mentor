e3593ee0560c8a73c9a1519b9cc393a1229563b3
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-21 09:16:20 +0000

    i created tech stack page

---
+++ src/Pages/TechStack/TechStackPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TechStackMain from '../../Components/TechStack/TechStackMain'
+
+const TechStackPage = () => {
+  return (
+      <>
+          <TechStackMain/>
+   </>
+  )
+}
+
+export default TechStackPage
+
