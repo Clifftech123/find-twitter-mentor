84e69308f2f6b4bfd1f87495a6152b51e8008169
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-21 09:15:33 +0000

    I created communities component

---
+++ src/Components/Communities/CommunitiesMain.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const CommunitiesMain = () => {
+  return (
+    <div className='container text-center'>
+       Welcome to Communities page Still working on this page
+    </div>
+  )
+}
+
+export default CommunitiesMain;
+
+
