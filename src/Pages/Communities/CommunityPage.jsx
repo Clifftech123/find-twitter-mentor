be47bacc6532676467ca2c12b07db8433a1ca152
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-21 09:18:05 +0000

    I Created communities Page

---
+++ src/Pages/Communities/CommunityPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommunitiesMain from '../../Components/Communities/CommunitiesMain'
+
+const CommunityPage = () => {
+  return (
+      <>
+          <CommunitiesMain/>
+   </>
+  )
+}
+
+export default CommunityPage
+
