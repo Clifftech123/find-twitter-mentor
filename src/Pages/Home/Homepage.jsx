4a3d7c509b944ea2a00048bc8d696b112c3b5e73
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-22 12:30:15 +0000

    Rnder of insprational content

--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -1,15 +1,17 @@
 // Ths components  render all the homepage content
 
 import React from "react";
-import FirstCards from "../../Components/Content/Firstcards";
+import FirstCards from "../../Components/HomeContent/Firstcards";
 import Hero from "../../Components/HeroSection/Hero";
+import Inspiration from "../../Components/HomeContent/Inspiration";
 
 const HomePage = () => {
 	return (
 		<>
 			<div className=" font-Poppins dark:bg-slate-900 duration-100">
 				<Hero />
-				<FirstCards/>
+				<FirstCards />
+				<Inspiration/>
 			</div>
 		</>
 	);
