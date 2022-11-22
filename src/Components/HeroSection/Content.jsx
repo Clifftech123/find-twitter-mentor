d7c70f8565148b79783cf9a133be7bb20990443f
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-22 12:24:49 +0000

    Changing  the text color

--- src/Components/HeroSection/Content.jsx
+++ src/Components/HeroSection/Content.jsx
@@ -3,19 +3,19 @@ import HereContentImage from '../../assets/WebDevolopmentPng.png'
 
 const Content = () => {
     return (
-			<section class="bg-white dark:bg-gray-900">
-				<div class="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16            lg:flex-row  lg:items-center">
-					<div class="w-full lg:w-1/2">
-						<div class="lg:max-w-lg">
-							<h1 class="text-3xl font-bold tracking-wide text-gray-600 dark:text-white lg:text-5xl">
+			<section className="bg-white dark:bg-gray-900">
+				<div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16            lg:flex-row  lg:items-center">
+					<div className="w-full lg:w-1/2">
+						<div className="lg:max-w-lg">
+							<h1 className="text-3xl font-bold tracking-wide text-gray-600 dark:text-white lg:text-5xl">
 								Easiest way to start your Tech Journey
 							</h1>
 							{/* what we offer to users and how we can help them to start their tech journey. */}
-							<div class="mt-8 space-y-5">
-								<p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
+							<div className="mt-8 space-y-5">
+								<p className="flex items-center -mx-2 text-gray-500 dark:text-gray-200">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										class="w-6 h-6 mx-2 text-blue-500"
+										className="w-6 h-6 mx-2 text-blue-500"
 										fill="none"
 										viewBox="0 0 24 24"
 										stroke="currentColor"
@@ -28,13 +28,13 @@ const Content = () => {
 										/>
 									</svg>
 
-									<span class="mx-2">Find Mentor</span>
+									<span className="mx-2">Find Mentor</span>
 								</p>
 
-								<p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
+								<p className="flex items-center -mx-2 text-gray-500 dark:text-gray-200">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										class="w-6 h-6 mx-2 text-blue-500"
+										className="w-6 h-6 mx-2 text-blue-500"
 										fill="none"
 										viewBox="0 0 24 24"
 										stroke="currentColor"
@@ -47,13 +47,13 @@ const Content = () => {
 										/>
 									</svg>
 
-									<span class="mx-2">Get Start with Tech Stack </span>
+									<span className="mx-2">Get Start with Tech Stack </span>
 								</p>
 
-								<p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
+								<p className="flex items-center -mx-2 text-gray-500 dark:text-gray-200">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										class="w-6 h-6 mx-2 text-blue-500"
+										className="w-6 h-6 mx-2 text-blue-500"
 										fill="none"
 										viewBox="0 0 24 24"
 										stroke="currentColor"
@@ -66,12 +66,12 @@ const Content = () => {
 										/>
 									</svg>
 
-									<span class="mx-2">Start Build Your Dream Project </span>
+									<span className="mx-2">Start Build Your Dream Project </span>
 								</p>
-								<p class="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
+								<p className="flex items-center -mx-2 text-gray-500 dark:text-gray-200">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										class="w-6 h-6 mx-2 text-blue-500"
+										className="w-6 h-6 mx-2 text-blue-500"
 										fill="none"
 										viewBox="0 0 24 24"
 										stroke="currentColor"
@@ -91,9 +91,9 @@ const Content = () => {
 					</div>
 					{/* end of the offer  */}
 					{/* Content image  */}
-					<div class="flex items-center justify-center w-full h-96 lg:w-1/2">
+					<div className="flex items-center justify-center w-full h-96 lg:w-1/2">
 						<img
-							class="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl"
+							className="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl"
 							src={HereContentImage}
 						/>
 					</div>
