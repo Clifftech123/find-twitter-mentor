68e17f4c70c6f3316e3fa9d15664e19377f8e811
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2022-11-28 15:49:57 +0000

    rendering tech stack component

--- src/Components/TechStack/TechStackMain.jsx
+++ src/Components/TechStack/TechStackMain.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Content from './Content'
 import TechStackHero from './Hero'
+import Stacks from './Stacks'
 
 const TechStackMain = () => {
   return (
     <>
       <TechStackHero />
-      <Content/>
+      <Content />
+      <Stacks/>
      </>
   )
 }
