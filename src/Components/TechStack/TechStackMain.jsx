3c9c8f31e07f776bf7abc2b993e0ecd738b7b5e6
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2022-12-13 21:47:47 +0000

    Rending of new quote content

--- src/Components/TechStack/TechStackMain.jsx
+++ src/Components/TechStack/TechStackMain.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Content from './Content'
 import TechStackHero from './Hero'
+import QuoteSwiper from './QuoteSwiper'
 import Stacks from './Stacks'
 
 
@@ -9,7 +10,8 @@ const TechStackMain = () => {
 		<>
 			<TechStackHero />
 			<Content />
-			<Stacks />
+		  <Stacks />
+		  <QuoteSwiper/>
 
 		
 		</>
