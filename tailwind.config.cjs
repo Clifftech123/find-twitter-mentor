0154f9d13ac4b3838794cccffd3282cdebbbe4b9
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2022-12-21 18:13:49 +0000

    Adding of tailwind pluging

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
 	mode: "jit",
 	darkMode: "class",
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}" ],
 	theme: {
 		extend: {
 			fontFamily: {
@@ -24,5 +24,17 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("daisyui")],
+	plugins: [ require( "daisyui" ) ],
+	// daisyUI config (optional)
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	},
+
 };
