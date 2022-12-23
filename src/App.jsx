0c65e4ad2d3faef50bb0c57bf7199609aeb39f2f
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2022-12-23 18:08:30 +0000

    Adding of routing of the Resources

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,25 @@ import FindMentor from "./Pages/FindMentor/FindMentor";
 import CommunityPage from "./Pages/Communities/CommunityPage";
 import TechStackPage from "./Pages/TechStack/TechStackPage";
 import Footer from "./Components/Footer/Footer";
+import Resources from "./Pages/Resources/Resources";
 
 const App = () => {
 	return (
 		<>
-			<div className="font-Poppins  dark:bg-Dark_bg_color" >
-				{/* MAIN NAVBAR ON THE PAGE */}
+			<div className="font-Poppins  dark:bg-Dark_bg_color">
+				{/* NavBar component.The NavBar component isn't part of the routing because we want to show it on all  the pages  */}
 				<Nav />
+				{/* Routing for all the pages  */}
 				<Routes>
 					<Route exact path="/" element={<HomePage />} />
 					<Route exact path="/fineMentor" element={<FindMentor />} />
 					<Route exact path="/communities" element={<CommunityPage />} />
 					<Route exact path="/techStack" element={<TechStackPage />} />
+					<Route exact path="/resources" element={ <Resources/>  } />
 				</Routes>
-				{/* FOOTER */}
-				
-					<Footer />
-				
+				{/* FOOTER component.The footer component isn't part of the routing because we want to show it on all  the pages  */}
+
+				<Footer />
 			</div>
 		</>
 	);
